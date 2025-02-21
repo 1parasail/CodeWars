196363a0be7ec0afc03e3180f0e84c2ace30a3a4
parasaill <lasvegas2992@gmail.com>
2025-02-21 09:10:15 +0100

    Add solution to task <<Bit Counting>>

--- 6kyu/Solution2.java
+++ 6kyu/Solution2.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+//Bit Counting
+public class Solution2
+{
+  public static void main (String[] args)
+  {
+      int zahl = 5;
+      System.out.println(countBits(zahl));
 
-public class Solution2 {
+  }
+
+    public static int countBits(int n){
+
+        int count = 0;
+        String bits = "";
+        while (n!=0)
+        {
+            bits+=String.valueOf(n%2);
+            n/=2;
+        }
+        for (int i = 0; i < bits.length(); i++)
+        {
+            if (bits.charAt(i)=='1')
+            {
+                count+=1;
+            }
+        }
+        return count;
+    }
 }
