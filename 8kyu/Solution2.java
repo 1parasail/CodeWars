e071cc195b97d82a9a8c727d6555bc3466799fe6
parasaill <lasvegas2992@gmail.com>
2025-02-22 15:43:41 +0100

    Add solution for task<<Multiply>>

---
+++ 8kyu/Solution2.java
@@ -0,0 +1,13 @@
+//Multiply
+public class Solution2
+{
+    public static void main (String[] args)
+    {
+        System.out.println(multiply(1.0,3.0));
+    }
+
+    public static Double multiply(Double a, Double b)
+    {
+        return a * b;
+    }
+}
